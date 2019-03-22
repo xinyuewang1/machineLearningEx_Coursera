a3d4e85d7f6a11f6262e232595f048cdc5454c07
xinyuewang1 <xinyue.wang1@ucdconnect.ie>
2019-03-22 19:07:49 +0000

    Implemented estimateGaussian, sigma2 with both var and homemade func

--- machine-learning-ex8/ex8/estimateGaussian.m
+++ machine-learning-ex8/ex8/estimateGaussian.m
@@ -21,10 +21,10 @@ sigma2 = zeros(n, 1);
 %               should contain variance of the i-th feature.
 %
 
-
-
-
-
+mu = sum(X)' / m;  
+%sigma2 = sum((X - mu') .^ 2)' / m; 
+% var: calculates the variance.
+sigma2 = var(X, opt = 1);
 
 
 
