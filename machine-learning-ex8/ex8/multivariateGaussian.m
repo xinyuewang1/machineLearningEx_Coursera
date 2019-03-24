71d1d526441045e8d2845f80f0dfa11474372c8d
xinyuewang1 <xinyue.wang1@ucdconnect.ie>
2019-03-24 13:38:54 +0000

    Implemented select threshold.

--- machine-learning-ex8/ex8/multivariateGaussian.m
+++ machine-learning-ex8/ex8/multivariateGaussian.m
@@ -12,11 +12,12 @@ function p = multivariateGaussian(X, mu, Sigma2)
 k = length(mu);
 
 if (size(Sigma2, 2) == 1) || (size(Sigma2, 1) == 1)
+  % sigma is vector
     Sigma2 = diag(Sigma2);
 end
 
 X = bsxfun(@minus, X, mu(:)');
 p = (2 * pi) ^ (- k / 2) * det(Sigma2) ^ (-0.5) * ...
     exp(-0.5 * sum(bsxfun(@times, X * pinv(Sigma2), X), 2));
-
+    % The formula mentioned in video
 end
